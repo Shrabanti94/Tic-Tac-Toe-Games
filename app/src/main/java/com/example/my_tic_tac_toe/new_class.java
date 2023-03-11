da5ed4f5ade38ee889f4dd8203e868f6e504284d
unknown <urmi1907094@stud.kuet.ac.bd>
2023-03-11 17:05:21 +0600

    try to conflict

--- app/src/main/java/com/example/my_tic_tac_toe/new_class.java
+++ app/src/main/java/com/example/my_tic_tac_toe/new_class.java
@@ -1,5 +1,9 @@
 package com.example.my_tic_tac_toe;
 
 public class new_class {
-    //try to add
+    public static void main(String args[])
+    {
+        System.out.print("Hello!");
+        System.out.print("Java");
+    }
 }
