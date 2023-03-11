285b9f213ed6638bbfa46e8ff87cf1d032db5a39
unknown <urmi1907094@stud.kuet.ac.bd>
2023-03-11 16:40:19 +0600

    add getter setter constructor

--- app/src/main/java/com/example/my_tic_tac_toe/newfile.java
+++ app/src/main/java/com/example/my_tic_tac_toe/newfile.java
@@ -1,10 +1,26 @@
 package com.example.my_tic_tac_toe;
 
 public class newfile {
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome");
-        System.out.println("To");
-        System.out.println("GeeksforGeeks");
+    private String name;
+    private int age;
+    public int getAge() {
+        return age;
+    }
+    newfile(){
+        
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void display() {
+        System.out.println("name: "+getName());
+        System.out.println("age: "+getAge());
     }
 }
+
